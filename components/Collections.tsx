6f9a20a0cfda0875f68e16d86939859b8b730f73
Matias Rivera Zahn <160002289+MatiasRZ7@users.noreply.github.com>
2024-05-09 18:54:17 +0200

    cambios 2

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -1,29 +1,17 @@
-"use client"
 import { getCollections } from "@/lib/actions/actions";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
 
-const Collections = () => {
-  const [collections, setCollections] = useState([]);
-
-  useEffect(() => {
-    const fetchCollections = async () => {
-      const result = await getCollections();
-      setCollections(result);
-    };
-
-    fetchCollections();
-  }, []);
+const Collections = async () => {
+  const collections = await getCollections();
 
   return (
     <div className="flex flex-col items-center gap-10 py-8 px-5">
       <p className="text-heading1-bold">Collections</p>
-
       {!collections || collections.length === 0 ? (
         <p className="text-body-bold">No collections found</p>
       ) : (
-        <div className="flex items-center justify-center gap-8">
+        <div className="flex flex-wrap items-center justify-center gap-8">
           {collections.map((collection: CollectionType) => (
             <Link href={`/collections/${collection._id}`} key={collection._id}>
               <Image
