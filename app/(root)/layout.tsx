86f0becc3f110b4727b5b1e538291add5b2feb19
Matias Rivera Zahn <160002289+MatiasRZ7@users.noreply.github.com>
2024-06-15 17:07:08 +0200

    Google tag manager added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -23,8 +23,25 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/logofav.ico" />
+        <Script strategy="lazyOnload">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MFRQKR8P');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MFRQKR8P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ClerkProvider>
           <ToasterProvider />
           <Navbar />
