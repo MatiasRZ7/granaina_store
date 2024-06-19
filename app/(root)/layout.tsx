eba50b9249a4ae37ed1d7ed5abf8cf24c4a5143d
Matias Rivera Zahn <160002289+MatiasRZ7@users.noreply.github.com>
2024-06-19 17:33:21 +0200

    prueba gtm 1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
 import ToasterProvider from "@/lib/providers/ToasterProvider";
 import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,15 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/logofav.ico" />
-        <Script strategy="afterInteractive">
-          {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PPZD4BQ5');
-         `}
-        </Script>
+        <GoogleTagManager gtmId="GTM-PPZD4BQ5" />
       </head>
       <body className={inter.className}>
         <noscript>
--- app/(root)/payment_success/page.tsx
+++ app/(root)/payment_success/page.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import useCart from "@/lib/hooks/useCart";
 import Link from "next/link";
 import { useEffect } from "react";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const SuccessfulPayment = () => {
   const cart = useCart();
@@ -14,18 +14,7 @@ const SuccessfulPayment = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center gap-5">
       <Head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16611348871"
-          strategy="afterInteractive"
-        />
-        <Script strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-16611348871');
-          `}
-        </Script>
+        <GoogleTagManager gtmId="AW-16611348871" />
       </Head>
       <p className="text-heading4-bold text-red-1">
         Pago efectuado correctamente.
