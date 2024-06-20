45e7b41efa5ca125e64f7f70a51d1bfe2c9a5858
Matias Rivera Zahn <160002289+MatiasRZ7@users.noreply.github.com>
2024-06-20 19:50:18 +0200

    gtm prueba 8

--- app/(root)/payment_success/page.tsx
+++ app/(root)/payment_success/page.tsx
@@ -3,8 +3,6 @@ import Head from "next/head";
 import useCart from "@/lib/hooks/useCart";
 import Link from "next/link";
 import { useEffect } from "react";
-import { GoogleTagManager } from "@next/third-parties/google";
-import Script from "next/script";
 
 const SuccessfulPayment = () => {
   const cart = useCart();
@@ -14,31 +12,6 @@ const SuccessfulPayment = () => {
   }, []);
   return (
     <div className="h-screen flex flex-col justify-center items-center gap-5">
-      <Head>
-        <GoogleTagManager gtmId="AW-16611348871" />
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16611348871"
-        ></Script>
-        <Script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-16611348871');
-          `}
-        </Script>
-        <Script>
-          {`
-            gtag('event', 'conversion', {
-              'send_to': 'AW-16611348871/Mb92CKGl0LoZEIej9PA9',
-              'value': 1.0,
-              'currency': 'MXN',
-              'transaction_id': ''
-            });
-          `}
-        </Script>
-      </Head>
       <p className="text-heading4-bold text-red-1">
         Pago efectuado correctamente.
       </p>
