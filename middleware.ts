d737b3497bce220ad0237ccd61f79f3fb822f773
Matias Rivera Zahn <160002289+MatiasRZ7@users.noreply.github.com>
2024-11-18 19:53:11 +0100

    prueba i g 9

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/:path*", "/api/public", "/api/public/*"],
+  publicRoutes: ["/:path*"],
 });
 
 export const config = {
