0645d7e4092a78fd24d298d28b639b1d5ac3f284
Matias Rivera Zahn <160002289+MatiasRZ7@users.noreply.github.com>
2024-11-18 19:34:44 +0100

    prueba index google 5

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/:path*", "/api/public"],
+  publicRoutes: ["/", "/:path*", "/api/public", "/api/public/*"],
 });
 
 export const config = {
