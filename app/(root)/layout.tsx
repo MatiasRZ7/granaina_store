a370100f6a706008881c8b5c030ea380a070d00c
Matias Rivera Zahn <160002289+MatiasRZ7@users.noreply.github.com>
2024-11-18 10:32:59 +0100

    prueba index google 1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -14,7 +14,6 @@ export const metadata: Metadata = {
     "Riviera Maya, Mexico, Tours, Excursions, Vacations, Ecotourism, Playa del Carmen, Tulum, Cancun, Cozumel, Cenotes, Mayan Ruins, Chichen Itza, Coba, Theme Parks, Xcaret, Xel-Ha, Xplor, Snorkeling, Scuba Diving, Nature Tours, Sustainable Tourism, Wildlife Sanctuaries, Family Vacations, Romantic Getaways",
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -24,14 +23,29 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/logofav.ico" />
-
+        <meta name="description" content="Tours in Riviera Maya, Mexico" />
+        <meta
+          name="keywords"
+          content="Riviera Maya, Mexico, Tours, Excursions, Vacations, Ecotourism, Playa del Carmen, Tulum, Cancun, Cozumel, Cenotes, Mayan Ruins, Chichen Itza, Coba, Theme Parks, Xcaret, Xel-Ha, Xplor, Snorkeling, Scuba Diving, Nature Tours, Sustainable Tourism, Wildlife Sanctuaries, Family Vacations, Romantic Getaways"
+        />
+        <meta
+          name="google-site-verification"
+          content="K7WSnm8Cp6EiR-DZn7gQ8DKxGW7cW_SD34x132S1TyI"
+        />
+        <title>Riviera Maya Tour</title>
       </head>
       <body className={inter.className}>
         <ClerkProvider>
           <ToasterProvider />
           <LanguageProvider>
-          <Navbar />
-          {children}
+            <Navbar />
+            <main>
+              <h1 className="sr-only">Riviera Maya Tour</h1>
+              <h2 className="sr-only">
+                Explore the Best Tours in Riviera Maya
+              </h2>
+              {children}
+            </main>
           </LanguageProvider>
         </ClerkProvider>
       </body>
