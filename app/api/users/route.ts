acc4a8cc61450185b38c1aa24b6a713bc6566838
Matias Rivera Zahn <160002289+MatiasRZ7@users.noreply.github.com>
2024-11-18 19:39:36 +0100

    prueba index google 6

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,30 +1,31 @@
 import User from "@/lib/models/User";
 import { connectToDB } from "@/lib/mongoDB";
 import { auth } from "@clerk/nextjs";
-
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   try {
-    const { userId } = auth()
+    const { userId } = auth();
 
     if (!userId) {
-      return new NextResponse(JSON.stringify({ message: "Not authorized" }), { status: 401 })
+      return new NextResponse(JSON.stringify({ message: "Not authorized" }), {
+        status: 401,
+      });
     }
 
-    await connectToDB()
+    await connectToDB();
 
-    let user = await User.findOne({ clerkId: userId })
+    let user = await User.findOne({ clerkId: userId });
 
     // Create a new user if not found
     if (!user) {
-      user = await User.create({ clerkId: userId })
-      await user.save()
+      user = await User.create({ clerkId: userId });
+      await user.save();
     }
 
-    return NextResponse.json(user, { status: 200 })
+    return NextResponse.json(user, { status: 200 });
   } catch (err) {
-    console.log("[users_GET]", err)
-    return new NextResponse("Internal Server Error", { status: 500 })
+    console.log("[users_GET]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
-}
+};
